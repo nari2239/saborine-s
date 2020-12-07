52d1cee0f52663a609506aea0a56a8c21b41589f
nari2239 <oryolife@gmail.com>
2020-12-07 20:23:18 +0900

    FactoryBotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {abc123}
+    password              {"abc123"}
     password_confirmation {password}
   end
 end

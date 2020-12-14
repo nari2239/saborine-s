be41e93d1fe94c45059ea38afc5c91d751a7fa73
nari2239 <oryolife@gmail.com>
2020-12-14 18:15:52 +0900

    新規記録保存処理

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,19 @@
 class RecordsController < ApplicationController
   def index
   end
+
+  def create
+    # binding.pry
+    # @record = Record.new(time: params[:time], skip: params[:skip], to_do: params[:to_do], user_id: current_user.id)
+    @record= Record.new(record_params)
+    if @record.valid?
+      @record.save
+      redirect_to user_path(current_user)
+    end
+  end
+
+  private
+  def record_params
+    params.permit(:time, :skip, :to_do).merge(user_id: current_user.id)
+  end
 end

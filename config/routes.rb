c0b8baa75719055e68a303addcde948f3b9ef2be
nari2239 <oryolife@gmail.com>
2020-12-14 20:36:45 +0900

    edit,updateアクションへのパスを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "records#index"
-  resources :records, only: [:index, :create]
+  resources :records, only: [:index, :create, :edit , :update]
   resources :users, only: :show
 end

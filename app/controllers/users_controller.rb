18e95bea497b37483415669b97b0ad1b640f85f9
nari2239 <oryolife@gmail.com>
2020-12-15 19:18:40 +0900

    表示順を作成日順に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @records = @user.records.order("updated_at DESC")
+    @records = @user.records.order("created_at DESC")
     @record = Record.new
   end
 end

86662bf31e60bde909b3f5fb958e46d83a91f025
nari2239 <oryolife@gmail.com>
2020-12-15 16:25:24 +0900

    updateアクションの追加

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -6,7 +6,7 @@ class RecordsController < ApplicationController
   def create
     # binding.pry
     # @record = Record.new(time: params[:time], skip: params[:skip], to_do: params[:to_do], user_id: current_user.id)
-    @record= Record.new(record_params)
+    @record= Record.new(time: params[:time], skip: params[:skip], to_do: params[:to_do], user_id: current_user.id)
     if @record.valid?
       @record.save
       redirect_to user_path(current_user)
@@ -19,10 +19,17 @@ class RecordsController < ApplicationController
   end
 
   def update
+    @user = User.find(current_user.id)
+    @record = Record.find(params[:id])
+    if @record.update(record_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   private
   def record_params
-    params.permit(:time, :skip, :to_do).merge(user_id: current_user.id)
+    params.require(:record).permit(:time, :skip, :to_do).merge(user_id: current_user.id)
   end
 end

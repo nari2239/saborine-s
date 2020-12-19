5793394ccbc991c82c7d5e52a4424423dea6dbb6
nari2239 <oryolife@gmail.com>
2020-12-19 13:06:31 +0900

    dateカラムのバリデーション追加

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,5 @@
 class Record < ApplicationRecord
+  validates :date, presence: true
   validates :time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 1440, message: '0~1440分の範囲で記入してください。' } 
   validates :skip, length: { minimum: 0, maximum: 1000, message: '1000文字以内で記入してください。' }, allow_blank: true
   validates :to_do, length: { minimum: 0, maximum: 1000, message: '1000文字以内で記入してください。' }, allow_blank: true

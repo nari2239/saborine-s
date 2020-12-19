710d59072bf437b73d4aa09aa9c3c540d9f65525
nari2239 <oryolife@gmail.com>
2020-12-19 16:13:13 +0900

    リファクタリング(set_userの設置)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
   before_action :authenticate_user!, only: [:show]
   before_action :move_to_self_show, only: [:show]
 
   def show
-    @user = User.find(params[:id])
     @records = @user.records.order("date DESC")
     @record = Record.new
   end
 
+  
   private
-  def move_to_self_show
+  def set_user
     @user = User.find(params[:id])
+  end
+
+  def move_to_self_show
     if current_user.id != @user.id
       redirect_to user_path(current_user.id)
     end

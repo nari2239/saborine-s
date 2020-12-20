60de292f7a327527e8e02e892f76c0f8a6073ede
nari2239 <oryolife@gmail.com>
2020-12-20 18:05:45 +0900

    FactoryBotの用意

--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :record do
-    
+    date  {Faker::Date.between(from: '2020-01-01', to: '2020-12-31')}
+    time  {Faker::Number.between(from: 0, to: 100)}
+    skip  {Faker::Lorem.characters(number: 100)}
+    to_do {Faker::Lorem.characters(number: 100)}
+    association :user
   end
 end

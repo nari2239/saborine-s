f2a4119b2718bb086bb4ae04f27e83bc50ba8417
nari2239 <oryolife@gmail.com>
2020-12-27 12:29:53 +0900

    ユーザーログインの統合テスト、ユーザー新規登録のdescribe名も変更

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe "ユーザー新規登録", type: :system do
   before do
     @user = FactoryBot.build(:user)
   end
@@ -52,6 +52,53 @@ RSpec.describe "Users", type: :system do
       }.to change { User.count }.by(0)
       # 新規登録ページへ戻されることを確認する
       expect(current_path).to eq "/users"
+    end
+  end
+end
+
+RSpec.describe 'ユーザーログイン', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  context 'ログインできるとき' do
+    it '保存されているユーザーの情報と合致すればログインできる' do
+      # トップページに遷移する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ遷移する
+      visit new_user_session_path
+      # 正しいユーザー情報を入力する
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      # サインインボタンを押す
+      find('input[name="commit"]').click
+      # ユーザー詳細ページへ遷移へ遷移することを確認する
+      expect(current_path).to eq user_path(@user)
+      # ユーザー詳細ページにログアウトボタンがあることを確認する
+      expect(page).to have_content('ログアウト')
+      # 新規登録ページへ遷移するボタンと、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+
+  context 'ログインできないとき' do
+    it '保存されているユーザーの情報と合致しないとログインができない' do
+      # トップページに遷移する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ遷移する
+      visit new_user_session_path
+      # ユーザー情報を入力する
+      fill_in 'user_email', with: ""
+      fill_in 'user_password', with: ""
+      # サインインボタンを押す
+      find('input[name="commit"]').click
+      # ログインページへ戻されることを確認する
+      expect(current_path).to eq new_user_session_path
     end
   end
 end

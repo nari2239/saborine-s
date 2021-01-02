ba847dee02ef3319a0cd245432b1b0f62972dcdf
nari2239 <oryolife@gmail.com>
2021-01-02 17:36:48 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "records#index"
-  resources :records, only: [:index, :create, :edit , :update, :destroy]
+  resources :records, only: [:index, :create, :edit , :update, :destroy] do
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: :show
 end

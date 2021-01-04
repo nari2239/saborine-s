1e044d1b17b7c9f829e868700443df402b882238
nari2239 <oryolife@gmail.com>
2021-01-05 01:09:35 +0900

    skipカラムのNOT NULL制約を外した(バリデーションでは空白を許可しているため揃えたい)

--- db/migrate/20201206094413_create_records.rb
+++ db/migrate/20201206094413_create_records.rb
@@ -3,7 +3,7 @@ class CreateRecords < ActiveRecord::Migration[6.0]
     create_table :records do |t|
       t.date :date, null:false
       t.integer :time, null: false
-      t.string :skip, null: false
+      t.string :skip
       t.string :to_do
       t.timestamps
     end

f5201f77c1ed6b6ecdf4ec5cc259e9845837568a
nari2239 <oryolife@gmail.com>
2021-01-05 01:22:20 +0900

    seedファイルを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(
+  nickname: "sabotaro",
+  email: 'user@test.com',
+  password: 'testuser1'
+)
+
+20.times do
+  Record.create(
+    date: Faker::Date.between(from: '2021-01-01', to: '2021-03-31'),
+    time: Faker::Number.between(from: 0, to: 100),
+    skip: Faker::Lorem.characters(number: 100),
+    to_do: Faker::Lorem.characters(number: 100),
+    user_id: user.id
+  )
+end
